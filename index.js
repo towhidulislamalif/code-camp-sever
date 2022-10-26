43d7bf535a2cd9a2afbb5132a01e9ada8025b629
towhidulislamalif <towhidulislamalif@gmail.com>
2022-10-27 00:43:57 +0600

    career data updated Alhamdulillah

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express');
 
+const cors = require('cors');
+
+const career = require('./data/career.json');
+
 const app = express();
+
+app.use(cors());
 
 const port = 5000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/career', (req, res) => {
+  res.send(career);
+});
+
+app.get('/career/:id', (req, res) => {
+  const id = req.params.id;
+  const cr = career.find((c) => c.id === id);
+  res.send(cr);
 });
 
 app.listen(port, () => {
